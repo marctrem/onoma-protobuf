36763c9253a27ec4cf549557977d3e06c6f1b269
Marc-Andre Tremblay <marcandre.tr@gmail.com>
2018-07-30 21:54:33 -0700

    first commit

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to onoma-protobuf
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/onoma_protobuf/core_test.clj
@@ -0,0 +1,7 @@
+(ns onoma-protobuf.core-test
+  (:require [clojure.test :refer :all]
+            [onoma-protobuf.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
